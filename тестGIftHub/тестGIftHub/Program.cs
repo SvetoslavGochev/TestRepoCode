655c4c3c140a9339cc67dfbf119efa42e3ef7472
Svetoslav Gochev <svetoslav.gochev@gmail.com>
2020-01-23 17:02:46 +0200

    Initial code added

---
+++ тестGIftHub/тестGIftHub/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace тестGIftHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 5;
+            Console.WriteLine(a);
+
+        }
+    }
+}
